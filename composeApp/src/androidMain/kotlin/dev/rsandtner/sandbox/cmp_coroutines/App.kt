5ce3678f9de1db9229b44e271e3edd710c4376b3
rsandtner <rsandtner@apache.org>
2025-11-21 15:38:12 +0100

    initial project creation

---
+++ composeApp/src/androidMain/kotlin/dev/rsandtner/sandbox/cmp_coroutines/App.kt
@@ -0,0 +1,12 @@
+package dev.rsandtner.sandbox.cmp_coroutines
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun App() {
+    MaterialTheme {
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,31 @@
+rootProject.name = "cmp_coroutines"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+    }
+}
+
+include(":composeApp")
