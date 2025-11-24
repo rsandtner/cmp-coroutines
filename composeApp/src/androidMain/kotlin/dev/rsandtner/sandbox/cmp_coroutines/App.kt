d1099d9fd0e7d2b9c07784a267491a04ab86e77d
rsandtner <rsandtner@apache.org>
2025-11-24 23:36:54 +0100

    monitor network connectivity (part I)

--- composeApp/src/androidMain/kotlin/dev/rsandtner/sandbox/cmp_coroutines/App.kt
+++ composeApp/src/androidMain/kotlin/dev/rsandtner/sandbox/cmp_coroutines/App.kt
@@ -1,12 +1,14 @@
 package dev.rsandtner.sandbox.cmp_coroutines
 
+import android.content.Context
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
-fun App() {
+fun App(context: Context) {
     MaterialTheme {
+        NetworkMonitorScreen(context)
     }
 }
